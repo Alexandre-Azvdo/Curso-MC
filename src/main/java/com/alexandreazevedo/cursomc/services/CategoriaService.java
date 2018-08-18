029b6ae3b8b5dc794c810b98d87ee2f7b0979b38
Alexandre Azevedo <azvdo.ads@gmail.com>
2018-08-18 18:49:46 -0300

    Banco de dados H2 e criação automática da base de dados.

---
+++ src/main/java/com/alexandreazevedo/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.alexandreazevedo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandreazevedo.cursomc.domain.Categoria;
+import com.alexandreazevedo.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
