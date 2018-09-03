f297f9cfe8f9f24941f028d3e1e79a7b288812c6
Alexandre Azevedo <azvdo.ads@gmail.com>
2018-09-03 13:24:36 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/alexandreazevedo/cursomc/services/CategoriaService.java
+++ src/main/java/com/alexandreazevedo/cursomc/services/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alexandreazevedo.cursomc.domain.Categoria;
+import com.alexandreazevedo.cursomc.exceptions.ObjectNotFoundException;
 import com.alexandreazevedo.cursomc.repositories.CategoriaRepository;
 
 @Service
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
