911ab4cb467abb3da91b79e21a0f611259439a50
Alexandre Azevedo <azvdo.ads@gmail.com>
2018-09-07 18:07:46 -0300

    Estado e Cidade

--- src/main/java/com/alexandreazevedo/cursomc/CursomcApplication.java
+++ src/main/java/com/alexandreazevedo/cursomc/CursomcApplication.java
@@ -8,18 +8,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.alexandreazevedo.cursomc.domain.Categoria;
+import com.alexandreazevedo.cursomc.domain.Cidade;
+import com.alexandreazevedo.cursomc.domain.Estado;
 import com.alexandreazevedo.cursomc.domain.Produto;
 import com.alexandreazevedo.cursomc.repositories.CategoriaRepository;
+import com.alexandreazevedo.cursomc.repositories.CidadeRepository;
+import com.alexandreazevedo.cursomc.repositories.EstadoRepository;
 import com.alexandreazevedo.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
-	CategoriaRepository categoriaRepository;
-	
+	CategoriaRepository categoriaRepository;	
 	@Autowired
-	ProdutoRepository produtoRepository;
+	ProdutoRepository produtoRepository;	
+	@Autowired
+	EstadoRepository estadoRepository;	
+	@Autowired
+	CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -41,8 +48,22 @@ public class CursomcApplication implements CommandLineRunner{
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+				
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+	
+		Estado sp = new Estado(null, "São Paulo");
+		Estado mg = new Estado(null, "Minas Gerais");
+		
+		Cidade uberlandia = new Cidade(null, "Uberlândia", mg);
+		Cidade saopaulo = new Cidade(null, "São Paulo", sp);
+		Cidade campinas = new Cidade(null, "Campinas", sp);
+		
+		mg.getCidades().addAll(Arrays.asList(uberlandia));
+		sp.getCidades().addAll(Arrays.asList(saopaulo, campinas));
+		
+		estadoRepository.saveAll(Arrays.asList(sp, mg));
+		cidadeRepository.saveAll(Arrays.asList(uberlandia, saopaulo, campinas));
+	
 	}
 }
