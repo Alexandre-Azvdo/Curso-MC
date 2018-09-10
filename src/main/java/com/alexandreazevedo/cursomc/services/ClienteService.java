99d992b966c20cbe17224ec84602aebbe4dde3d5
Alexandre Azevedo <azvdo.ads@gmail.com>
2018-09-10 10:40:24 -0300

    Pedido, Estado Pagamneto e Pagamento

--- src/main/java/com/alexandreazevedo/cursomc/services/ClienteService.java
+++ src/main/java/com/alexandreazevedo/cursomc/services/ClienteService.java
@@ -5,10 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.alexandreazevedo.cursomc.domain.Categoria;
 import com.alexandreazevedo.cursomc.domain.Cliente;
 import com.alexandreazevedo.cursomc.exceptions.ObjectNotFoundException;
-import com.alexandreazevedo.cursomc.repositories.CategoriaRepository;
 import com.alexandreazevedo.cursomc.repositories.ClienteRepository;
 
 @Service
