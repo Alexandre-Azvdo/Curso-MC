a393725db3461ac455c469ba2247c6b17e9e0925
Alexandre Azevedo <azvdo.ads@gmail.com>
2018-10-16 16:28:11 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/alexandreazevedo/cursomc/resources/ClienteResource.java
+++ src/main/java/com/alexandreazevedo/cursomc/resources/ClienteResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alexandreazevedo.cursomc.domain.Cliente;
 import com.alexandreazevedo.cursomc.dto.ClienteDTO;
+import com.alexandreazevedo.cursomc.dto.ClienteNewDTO;
 import com.alexandreazevedo.cursomc.services.ClienteService;
 
 @RestController
@@ -36,7 +37,7 @@ public class ClienteResource {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto){
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
 		Cliente obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		// Pega a URI do novo recurso que foi inserido
---
+++ src/main/java/com/alexandreazevedo/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,40 @@
+package com.alexandreazevedo.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.alexandreazevedo.cursomc.domain.enums.TipoCliente;
+import com.alexandreazevedo.cursomc.dto.ClienteNewDTO;
+import com.alexandreazevedo.cursomc.resources.excepiton.FieldMessage;
+import com.alexandreazevedo.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		// inclua os testes aqui, inserindo erros na lista
+		if(objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CPF Inválido."));
+		}
+		if(objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CNPJ Inválido."));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
