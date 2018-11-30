97ce13d880a1d90e1ded24b3472f07f1e1acab4b
Alexandre Azevedo <azvdo.ads@gmail.com>
2018-11-30 19:33:32 -0300

    Adicionando senha a Cliente

--- src/main/java/com/alexandreazevedo/cursomc/services/DBService.java
+++ src/main/java/com/alexandreazevedo/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.alexandreazevedo.cursomc.domain.Categoria;
@@ -51,6 +52,8 @@ public class DBService {
 	PagamentoRepository pagamentoRepository;
 	@Autowired
 	ItemPedidoRepository itemPedidoRepository;
+	@Autowired
+	private BCryptPasswordEncoder pe;
 
 	public void instantiateTestDataBase() throws ParseException {
 		Categoria cat1 = new Categoria(null, "Informática");
@@ -110,7 +113,7 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(uberlandia, saopaulo, campinas));
 	
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "azvdo.ads@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "azvdo.ads@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto", "Jardim", "38220834", cli1, uberlandia);
